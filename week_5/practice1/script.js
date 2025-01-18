913a6b11c6617a6df41bb4ae664ce53a960078b6
Saba Tahmasebinezhad <110444941+sabatms@users.noreply.github.com>
2025-01-18 00:14:36 -0800

    Add files via upload

---
+++ week_5/practice1/script.js
@@ -0,0 +1,13 @@
+const name1 = " milad.botostart@gmail.com ";
+const part1 = name1.split("@");
+const final1 = part1[0];
+console.log(final1);
+// Or =>   const part1 = name1.split("@")[0];
+// console.log(part1)
+
+const name2 = " erfan_yusefi@gmail.com ";
+const part2 = name2.split("@");
+const final2 = part2[0];
+console.log(final2);
+// Or => const part2 = name2.split("@")[0];
+// console.log(part2);
---
+++ week_5/practice2/script2.js
@@ -0,0 +1,11 @@
+const phoneNumbers = [
+  "+989393676765",
+  "+989393456123",
+  "+989393455465",
+  "+989393456722",
+  "+989393456765",
+];
+
+const phone1 = phoneNumbers[2];
+const finalphone = "0" + phone1.slice(3);
+console.log(finalphone);
