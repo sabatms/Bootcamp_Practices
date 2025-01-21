6d8c72d02ef6ea0ba3e016af24b0074e103ed12f
Saba Tahmasebinezhad <saba.tms15@gmail.com>
2025-01-21 12:30:05 +0330

    add script.js practice1 file

--- week7/practice1/script1.js
+++ week7/practice1/script1.js
@@ -1,13 +1,12 @@
 const numbers = [5, 12, 8, 130, 44];
 
 const sortDescending = [...numbers].sort(( a, b) => b-a);
-console.log("ارایه به صورت نزولی : ," , sortDescending );
-
+console.log("ارایه به صورت نزولی : " , sortDescending );
 
 const lessThan30 = numbers.filter(num1 => num1 < 30);
 console.log("محاسبه اعدا کمتر از 30 :" , lessThan30);
 
-const sumOfallnumbers = numbers.map((sum,num) => sum + num , 0);
+const sumOfallnumbers = numbers.reduce((sum,num) => sum + num , 0);
 console.log('Numbers جمع تمام ارایه های داخل :' , sumOfallnumbers);
 
 const stringifNumber = numbers.map(num => num.toString());
