6299437d2f91d2fec6ca6c689900f5173b38e91e
Saba Tahmasebinezhad <saba.tms15@gmail.com>
2025-01-21 11:41:26 +0330

    add script2.js practice2 file

---
+++ week7/practice2/script2.js
@@ -0,0 +1,12 @@
+function getweekday(date){
+
+    const weekdays = ['Sunday' , 'Monday', 'Tuseday' ,'Wednesday' ,'Thursday' ,'Friday' ,'Saturday'];
+
+    const dayIndex = date.getDay();
+
+    return weekdays[dayIndex];
+}
+
+console.log(getweekday(new Date()));
+console.log(getweekday(new Date ('2012-10--10')));
+
